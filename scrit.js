f629566243109ad52f17b3a48fac51ab6e8091c2
OmarNino11 <omar.nino.parodi11@gmail.com>
2024-12-24 00:43:58 -0500

    Update scrit.js

--- scrit.js
+++ scrit.js
@@ -1,14 +1,13 @@
-// Selecciona todos los botones de los certificados
-const botones = document.querySelectorAll(".ver-certificado");
-
-// Agrega el evento 'click' a cada botón
-botones.forEach((boton) => {
-    boton.addEventListener("click", () => {
-        // Obtén el documento desde el atributo 'data-documento'
-        const documento = boton.getAttribute("data-documento");
-
-        // Abre el documento en una nueva pestaña
-        window.open(documento, "_blank");
-    });
-});
-
+function mostrarCertificado(rutaImagen) {
+    const ventanaCertificado = window.open("", "Certificado", "width=800,height=600");
+    ventanaCertificado.document.write(`
+        <html>
+            <head>
+                <title>Certificado</title>
+            </head>
+            <body style="display: flex; justify-content: center; align-items: center; height: 100%; margin: 10;">
+                <img src="${rutaImagen}" alt="Certificado" style="max-width: 200%; max-height: 100%;">
+            </body>
+        </html>
+    `);
+}
